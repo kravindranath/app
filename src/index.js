9a55d0e8070c5428826804897e3ada899943df4a
Ravindranath K <ravindranath.k@espn.com>
2020-08-18 17:37:35 +0530

    wip

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function Home(){
-    return(
-        <h2>Home: Hello World!</h2>
-    );
-}
+import Home from './Home';
 
 
 ReactDOM.render(<Home />, document.getElementById('app'));
